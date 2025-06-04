df7058c105aabff049ef3e09d55789df43ca52fd
PERPELEX <hannanamir18@gmail.com>
2025-06-04 12:21:23 +0500

    Tittle added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Dynavents - Event Manager</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/images/icon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* <title>Dynavents - Event Manager</title>
+        <link rel="icon" href="./images/icon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
